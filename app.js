b67d8543950b5bee53f6d5571bd59d1af10029d5
MALHAR <malhar131005@gmail.com>
2025-02-19 18:43:32 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let totaram = document.querySelector(".aOfTotaram");  // Selects the button
 let totaramClass = document.querySelector(".totaramSetContainer"); // Selects the hidden container
+let totaramDiv = document.querySelector("#vithkid1") //selecyt the div for the totaram
 
 totaram.addEventListener("click", () => {  
 
     console.log("Button clicked!");
     totaramClass.classList.remove("hide");  // Removes the "hide" class
+    totaramDiv.classList.add("after")
+
 });
