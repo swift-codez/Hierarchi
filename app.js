e961dbb2ffdd8db7c6b1106f4dcfa99a831b0c66
MALHAR <malhar131005@gmail.com>
2025-02-19 12:57:27 +0530

    did some trial and error

---
+++ app.js
@@ -0,0 +1,7 @@
+let totaram = document.querySelector(".aOfTotaram");
+let totaramClass = document.querySelector(".totaramkids");
+
+totaram.addEventListener = ("click",()=> {
+
+    totaramClass.classList.remove("hide");
+})
